66483b9e6b6a43b58abefc072e538a15a0e68863
kaylee182000 <trungkien40414@gmail.com>
2022-07-29 21:02:21 +0700

    update

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function ProductItem({ prod }) {
+  return (
+    <div className="col-4 p-3">
+      <Link to={`/product/${prod.id}`}>
+        <img src={prod.image} width="100%" alt="" />
+      </Link>
+      <div className="p-3 text-center">
+        <h4>{prod.name}</h4>
+        <p>{prod.price}$</p>
+      </div>
+    </div>
+  );
+}
